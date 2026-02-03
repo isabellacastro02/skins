f43f8bf8c5e0d0aaa7ab5dc9d67f8c0010f099d0
isabellacastro02 <isabella.mcastro02@gmail.com>
2026-02-03 12:32:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className="auth-container">
+        <div className="auth-background">
+          
+        </div>
+      </div>
+    </>
+  )
+}
